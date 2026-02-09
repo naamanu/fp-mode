98a102dd41778aa7c585822726347a19b17389a5
Nana Adjei Manu <n.k.a.manu06@gmail.com>
2026-02-09 07:35:45 +0100

    chore: add some workspace stuff

---
+++ docs/.astro/content-assets.mjs
@@ -0,0 +1,4 @@
+
+import __ASTRO_IMAGE_IMPORT_1Uq8fr from "../../assets/hero-dark.svg?astroContentImageFlag=&importer=src%2Fcontent%2Fdocs%2Findex.mdx";
+export default new Map([["../../assets/hero-dark.svg?astroContentImageFlag=&importer=src%2Fcontent%2Fdocs%2Findex.mdx", __ASTRO_IMAGE_IMPORT_1Uq8fr]]);
+		
---
+++ docs/.astro/content-modules.mjs
@@ -0,0 +1,14 @@
+
+export default new Map([
+["src/content/docs/getting-started/quickstart.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fgetting-started%2Fquickstart.mdx&astroContentModuleFlag=true")],
+["src/content/docs/cli/commands.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fcli%2Fcommands.mdx&astroContentModuleFlag=true")],
+["src/content/docs/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Findex.mdx&astroContentModuleFlag=true")],
+["src/content/docs/getting-started/installation.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fgetting-started%2Finstallation.mdx&astroContentModuleFlag=true")],
+["src/content/docs/guide/docker-setup.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fguide%2Fdocker-setup.mdx&astroContentModuleFlag=true")],
+["src/content/docs/guide/solving-problems.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fguide%2Fsolving-problems.mdx&astroContentModuleFlag=true")],
+["src/content/docs/languages/haskell.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Flanguages%2Fhaskell.mdx&astroContentModuleFlag=true")],
+["src/content/docs/languages/ocaml.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Flanguages%2Focaml.mdx&astroContentModuleFlag=true")],
+["src/content/docs/problems/contributing.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fproblems%2Fcontributing.mdx&astroContentModuleFlag=true")],
+["src/content/docs/guide/progress-tracking.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fguide%2Fprogress-tracking.mdx&astroContentModuleFlag=true")],
+["src/content/docs/problems/overview.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fproblems%2Foverview.mdx&astroContentModuleFlag=true")]]);
+		
